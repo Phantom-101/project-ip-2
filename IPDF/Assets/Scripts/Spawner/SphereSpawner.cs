ba655e7400125d353feace473f8843ccaec4ece2
Phantom-101 <ccradical2468@gmail.com>
2020-05-05 17:00:43 -0700

    Added old assets

---
+++ IPDF/Assets/Scripts/Spawner/SphereSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour {
+    public GameObject sphere;
+
+    void Update()
+    {
+        Instantiate(sphere, transform.position, Quaternion.identity);
+    }
+}
